c9734e50fb1d177b4a93eb5c6ffdcd7ea664e217
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-03-08 19:26:08 +0200

    deploy3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,15 @@
-import * as functions from "firebase-functions";
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+import routes from "./routes/index";
+import { connectDb } from "./db";
+const app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(routes);
+connectDb().then(async () => {
+  app.listen(3001, () => console.log("Listening on http://localhost:3001"));
+});
+exports.app = functions.https.onRequest(app);
