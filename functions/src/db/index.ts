0d53e798b00490b1e52530fb4ad3bda7e20e7a86
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-05-01 12:39:28 +0300

    remove deployment

--- functions/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-import { MongoURI } from "../config/key";
-
-const connectDb = async () => {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(MongoURI);
-};
-
-export { connectDb };
--- functions/src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-import { AuthController } from "../controllers/AuthController";
-
-
-const router = express.Router();
-router.post("/register", AuthController.register);
-router.post("/login", AuthController.login);
-export default router;
